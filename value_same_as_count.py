06f19ae187d17408becb393bc90e8d9a93cde956
sowjanya003 <84395809+sowjanya003@users.noreply.github.com>
2022-07-11 08:23:25 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] c=0 for i in l:     if i not in l1:         l1.append(i) for i in l1:     if l.count(i)==i:         c+=1 print(c)

---
+++ value_same_as_count.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+c=0
+for i in l:
+    if i not in l1:
+        l1.append(i)
+for i in l1:
+    if l.count(i)==i:
+        c+=1
+print(c)
+   
+        
