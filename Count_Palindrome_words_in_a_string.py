11705d09a07b213c10f565344dbbc91d69996e80
sowjanya003 <84395809+sowjanya003@users.noreply.github.com>
2022-07-23 11:43:03 +0530

    def pl(i):     p=i.lower()     l=p[::-1]     if p==l:         return True     else:         return False n=input() l=n.split() c=0 for i in l:     if pl(i)==True:         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,7 +1,14 @@
-n=input().lower().split()
+def pl(i):
+    p=i.lower()
+    l=p[::-1]
+    if p==l:
+        return True
+    else:
+        return False
+n=input()
+l=n.split()
 c=0
-for i in n:
-    l=i[::-1]
-    if i==l:
+for i in l:
+    if pl(i)==True:
         c+=1
 print(c)
