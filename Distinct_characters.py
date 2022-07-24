5f0e3476796fc7dee727baa8a20bf5ee4bb9c273
sowjanya003 <84395809+sowjanya003@users.noreply.github.com>
2022-07-24 19:01:24 +0530

    n=input() l=list(n.lower()) l1=[] for i in range(len(l)):     if l[i]!=' ':         if l[i] not in l1:             l1.append(l[i])
    l1.sort()        
    print(''.join(l1))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -3,7 +3,7 @@ l=list(n.lower())
 l1=[]
 for i in range(len(l)):
     if l[i]!=' ':
-        if l.count(l[i])==1:
+        if l[i] not in l1:
             l1.append(l[i])
             
 l1.sort()        
