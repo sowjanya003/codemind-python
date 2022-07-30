245f10aa6c4c0797ee8ae587b36a65eea08e991f
sowjanya003 <84395809+sowjanya003@users.noreply.github.com>
2022-07-30 20:13:34 +0530

    def npd(a):     c=0     d=0     for j in range(1,a+1):         if a%j==0:             c+=1     if c!=2:         d+=1     return d n=int(input()) l=[] for i in range(1,n+1):     if n%i==0:         l.append(npd(i)) print(sum(l))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,11 +1,15 @@
+def npd(a):
+    c=0
+    d=0
+    for j in range(1,a+1):
+        if a%j==0:
+            c+=1
+    if c!=2:
+        d+=1
+    return d
 n=int(input())
-c=0
+l=[]
 for i in range(1,n+1):
     if n%i==0:
-        fc=0
-        for j in range(1,i+1):
-            if i%j==0:
-                fc+=1
-        if fc!=2:
-            c+=1
-print(c)
+        l.append(npd(i))
+print(sum(l))
